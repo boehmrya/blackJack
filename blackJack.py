311450f245c4aa2606beaba4d09586366353297d
Ryan Boehm <ryan.w.boehm@gmail.com>
2017-08-17 00:06:25 -0500

    implemented deck class and main blackjack function

--- blackJack.py
+++ blackJack.py
@@ -1,17 +1,21 @@
-# a simple black jack game to work on object oriented design
+# a simple black jack game
+
+import random
+
+# global variables
+suits = ['spades', 'clubs', 'diamonds', 'hearts']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'jack', 'queen', 'king', 'ace']
+scores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10', '10', '10']
 
 
 class Card:
-	suits = ['spades', 'clubs', 'diamonds', 'hearts']
-	ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'jack', 'queen', 'king', 'ace']
-
 	def __init__(self, isuit, irank):
 		self.suit = isuit
 		self.rank = irank
 
 	# to string method
 	def __str__(self):
-		name = suit + " " + rank
+		name = self.suit + " " + self.rank
 		return name
 
 	# greater than or equal to
@@ -92,7 +96,7 @@ class Card:
 		otherRankIndex = ranks.index(other.rank)
 		thisSuitIndex = suits.index(self.suit)
 		otherSuitIndex = suits.index(other.suit)
-		if (( thisRankIndex == otherRankIndex ) && ( thisSuitIndex == otherSuitIndex )) :
+		if (( thisRankIndex == otherRankIndex ) and ( thisSuitIndex == otherSuitIndex )):
 			return True
 		else:
 			return False
@@ -103,14 +107,66 @@ class Card:
 		otherRankIndex = ranks.index(other.rank)
 		thisSuitIndex = suits.index(self.suit)
 		otherSuitIndex = suits.index(other.suit)
-		if (( thisRankIndex != otherRankIndex ) || ( thisSuitIndex != otherSuitIndex )) :
+		if (( thisRankIndex != otherRankIndex ) or ( thisSuitIndex != otherSuitIndex )):
 			return True
 		else:
 			return False
 
 
 class Deck:
-	
+	def __init__(self):
+		self.cards = []
+		# add all cards in array in order
+		for suit in suits:
+			for rank in ranks:
+				card = Card(suit, rank)
+				self.cards.append(card)
+
+	# randomly re-order the cards in the deck
+	def shuffle(self):
+		random.shuffle(self.cards)
+
+	# randomly draw one card
+	def draw(self):
+		index = random.randint(0, 51)
+		card = self.cards[index]
+		return card
+
+
+def blackJack():
+	# initialize the deck of cards
+	gameDeck = Deck()
+	gameDeck.shuffle()
+
+	# draw first card
+	card1 = gameDeck.draw()
+	print("first card is: " + str(card1))
+	move = input("Stand or Hit? Enter h or s: ")
+
+	# player move
+	if move == 's':
+		print("game over")
+	elif move == 'h':
+		card2 = gameDeck.draw()
+		print("second card is: " + str(card2))
+		index1 = ranks.index(card1.rank)
+		index2 = ranks.index(card2.rank)
+		score1 = int(scores[index1])
+		score2 = int(scores[index2])
+		totalScore = score1 + score2
+		print("total score equals: " + str(totalScore))
+	else:
+		print("sorry.  you didn't enter h or s")
+
+# play game
+blackJack()
+
+
+
+
+
+
+
 
 
 
